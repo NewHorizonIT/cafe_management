cbe9550645d741d7f4bf4e105518fb71d13655f6
HuyPNT <huyphan15032005@gmail.com>
2025-04-13 13:04:44 +0700

    Update MaterialRepository.java

--- springboot/springboot/src/main/java/com/springboot/springboot/repository/MaterialRepository.java
+++ springboot/springboot/src/main/java/com/springboot/springboot/repository/MaterialRepository.java
@@ -1,1 +1,9 @@
+// MaterialRepository.java
+package com.springboot.springboot.repository;
 
+// Ensure the correct package path for Material
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.springboot.model.Material;
+
+public interface MaterialRepository extends JpaRepository<Material, Integer> {}
