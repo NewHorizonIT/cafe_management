71e89a960e6d371005a548f7b6c1b7efbebe3dbc
HuyPNT <huyphan15032005@gmail.com>
2025-04-13 19:14:25 +0700

    Create ReportController

--- springboot/springboot/src/main/java/com/springboot/springboot/controller/ReportController.java
+++ springboot/springboot/src/main/java/com/springboot/springboot/controller/ReportController.java
@@ -1,17 +1,18 @@
 // ReportController
 package com.springboot.springboot.controller;
 
-import com.springboot.springboot.model.Material;
-import com.springboot.springboot.model.Order;
-import com.springboot.springboot.model.Purchase;
-import com.springboot.springboot.service.ReportService;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.springboot.springboot.model.Material;
+import com.springboot.springboot.model.Order;
+import com.springboot.springboot.model.Purchase;
+import com.springboot.springboot.service.ReportService;
 
 @RestController
 @RequestMapping("/api/reports")
@@ -19,21 +20,27 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
-    // API thống kê tổng hợp: Bán hàng
+    // Xem toàn bộ báo cáo bán hàng
     @GetMapping("/orders")
     public List<Order> getAllOrders() {
         return reportService.getAllOrders();
     }
 
-    // API thống kê tổng hợp: Mua hàng
+    // Xem toàn bộ báo cáo mua hàng
     @GetMapping("/purchases")
     public List<Purchase> getAllPurchases() {
         return reportService.getAllPurchases();
     }
 
-    // API thống kê tổng hợp: Nhập kho
+    // Xem toàn bộ báo cáo nhập kho
     @GetMapping("/material")
     public List<Material> getAllMaterial() {
-        return reportService.getAllMaterial();
+        return reportService.getAllMaterials();
+    }
+
+    // Thống kê tổng hợp
+    @GetMapping("/stats")
+    public Map<String, Object> getStats() {
+        return reportService.getStats();
     }
 }
