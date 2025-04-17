e77c89b9316e79bc008988a5294448a7b9e61f1e
AnhQuan <nguyenanhquandev@gmail.com>
2025-04-17 19:43:07 +0700

    ui: fix ui management

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ({ links, socialLinks, logo }) => {
   return (
-    <footer class="bg-neutral text-neutral-content p-10 ">
+    <footer className="bg-neutral text-neutral-content p-10 ">
       <div className="max-w-[1180px] w-100% mx-auto grid grid-cols-1 sm:grid-cols-3 justify-between gap-5">
         <aside>
           <div>{logo}</div>
@@ -13,18 +13,22 @@ const Footer = ({ links, socialLinks, logo }) => {
           </p>
         </aside>
         <nav>
-          <h6 class="footer-title">Social</h6>
-          <div class="grid grid-flow-row gap-4">
-            {links.map((link) => (
-              <a href={link.link}>{link.lable}</a>
+          <h6 className="footer-title">Social</h6>
+          <div className="grid grid-flow-row gap-4">
+            {links.map((link, index) => (
+              <li key={index}>
+                <a href={link.href}>{link.label}</a>
+              </li>
             ))}
           </div>
         </nav>
         <nav>
-          <h6 class="footer-title">Social</h6>
-          <div class="flex gap-4">
-            {socialLinks.map((item) => (
-              <a href={item.link}>{item.icon}</a>
+          <h6 className="footer-title">Social</h6>
+          <div className="flex gap-4">
+            {socialLinks.map((item, i) => (
+              <a href={item.link} key={i}>
+                {item.icon}
+              </a>
             ))}
           </div>
         </nav>
